eab86dbc64bda1d7ef5d013c5e0ca4d1de1e30ef
saviobertolani <savio@setimaarte.com.br>
2025-09-23 09:53:53 -0300

    fix(mobile): importar assets via Vite e corrigir wrappers absolutos (sem contents) no MobileFigma; resolve erro server-fs-allow e layout quebrado

--- src/components/figma/MobileFigma.tsx
+++ src/components/figma/MobileFigma.tsx
@@ -1,27 +1,29 @@
 import React from "react";
 
+// Importação correta dos assets (Vite) — evita paths absolutos fora do allow list
+import imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D812 from "@/figma/mobile-assets/705cc0dc55d0d5a68388c772e2b6117b6c99cd0f.png";
+import imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D814 from "@/figma/mobile-assets/363d73ec33595dde3285b1f284098774998f5dac.png";
+import imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D815 from "@/figma/mobile-assets/012759f13f56517f1097b1fc4887ce210daa0750.png";
+import imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D816 from "@/figma/mobile-assets/8fca1f53bb6c91dfa7bb390285d6e25c4fdabb30.png";
+import imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D817 from "@/figma/mobile-assets/982534bd46b9e568691c0e5652a818ec5954a309.png";
+import imgEmCima from "@/figma/mobile-assets/c5bf77151a8866fa78db0df8dde0114c259578fa.png";
+import imgAquamarine41 from "@/figma/mobile-assets/6f46d9eac5f0e6717c6f749ddb7cef9b8551351a.png";
+import img250127AlmapStillTeraTheTownFrenteV081 from "@/figma/mobile-assets/0e4c4fe22e1cb5636a33462c5ac2022a711c0f36.png";
+import imgEmbaixo from "@/figma/mobile-assets/5112680c7f40d23f48f8876eb2dc44893c6f5a26.png";
+import imgIconeSeta from "@/figma/mobile-assets/7d81ad28ec29517142c61263569911e56b5bf8b1.svg";
+import imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D813 from "@/figma/mobile-assets/0aa0fffca858a707ff143e0af17fd4a0c981c966.svg";
+import imgBotao from "@/figma/mobile-assets/a4c6c6d2eaa486caf6f1c3c20b372c4a3e46e1e6.svg";
+import imgLogoSetima from "@/figma/mobile-assets/e3673b553c424c6b2648267f7e48b6843fc09b6b.svg";
+import imgLogoSetima1 from "@/figma/mobile-assets/7674cae785697e8381e7c52cd1935cc4128a49d2.svg";
+import imgGroup from "@/figma/mobile-assets/6225e7201df464b6a08b03339a7f5840b8630a79.svg";
+import imgLinkedin from "@/figma/mobile-assets/cee4173fa836977c0c829c200d5affe5043cbc21.svg";
+import img250127AlmapStillTeraTheTownFrenteV82 from "@/figma/mobile-assets/db7fef7f28a65df6eff5e06e19440a9b50550bf4.svg";
+import imgVwConstelattion313206X4CacambaPsb1 from "@/figma/mobile-assets/85739bc9aff55a55c211190c1a7ee5e3be0295e8.svg";
+
 // ATENÇÃO: Este componente foi gerado a partir do Figma (get_code)
 // Ele serve como baseline visual 1:1 do layout mobile para conferência.
 // Não está conectado ao fluxo real ainda; usaremos como referência.
 
-const imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D812 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/705cc0dc55d0d5a68388c772e2b6117b6c99cd0f.png";
-const imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D814 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/363d73ec33595dde3285b1f284098774998f5dac.png";
-const imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D815 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/012759f13f56517f1097b1fc4887ce210daa0750.png";
-const imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D816 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/8fca1f53bb6c91dfa7bb390285d6e25c4fdabb30.png";
-const imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D817 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/982534bd46b9e568691c0e5652a818ec5954a309.png";
-const imgEmCima = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/c5bf77151a8866fa78db0df8dde0114c259578fa.png";
-const imgAquamarine41 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/6f46d9eac5f0e6717c6f749ddb7cef9b8551351a.png";
-const img250127AlmapStillTeraTheTownFrenteV081 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/0e4c4fe22e1cb5636a33462c5ac2022a711c0f36.png";
-const imgEmbaixo = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/5112680c7f40d23f48f8876eb2dc44893c6f5a26.png";
-const imgIconeSeta = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/7d81ad28ec29517142c61263569911e56b5bf8b1.svg";
-const imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D813 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/0aa0fffca858a707ff143e0af17fd4a0c981c966.svg";
-const imgBotao = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/a4c6c6d2eaa486caf6f1c3c20b372c4a3e46e1e6.svg";
-const imgLogoSetima = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/e3673b553c424c6b2648267f7e48b6843fc09b6b.svg";
-const imgLogoSetima1 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/7674cae785697e8381e7c52cd1935cc4128a49d2.svg";
-const imgGroup = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/6225e7201df464b6a08b03339a7f5840b8630a79.svg";
-const imgLinkedin = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/cee4173fa836977c0c829c200d5affe5043cbc21.svg";
-const img250127AlmapStillTeraTheTownFrenteV82 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/db7fef7f28a65df6eff5e06e19440a9b50550bf4.svg";
-const imgVwConstelattion313206X4CacambaPsb1 = "/Users/saviobertolani/Downloads/Landing Page Sétima - NGI/src/figma/mobile-assets/85739bc9aff55a55c211190c1a7ee5e3be0295e8.svg";
 
 function BotaoCall() {
 	return (
@@ -46,8 +48,8 @@ interface BackMobileProps {
 function BackMobile({ propriedade1 = "Frame 2" }: BackMobileProps) {
 	return (
 		<div className="relative size-full" data-name="Propriedade 1=Frame 2" data-node-id="1:270">
-			<div className="absolute contents left-0 top-0" data-node-id="1:196">
-				<div className="absolute contents left-0 top-0" data-name="Mask group" data-node-id="1:177">
+				<div className="absolute left-0 top-0" data-node-id="1:196">
+					<div className="absolute left-0 top-0" data-name="Mask group" data-node-id="1:177">
 					<div
 						className="absolute bg-center bg-cover bg-no-repeat h-[1008px] left-[-559px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[559px_58px] mask-size-[402px_874px] top-[-58px] w-[1577px]"
 						data-name="ai_tools._Waist-up_portrait_American_shot_of_a_smiling_person_w_47b472c7-205a-4d66-bacf-38f70faa6d81 2"
@@ -55,7 +57,7 @@ function BackMobile({ propriedade1 = "Frame 2" }: BackMobileProps) {
 						style={{ backgroundImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D812}')`, maskImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D813}')` }}
 					/>
 				</div>
-				<div className="absolute contents left-[432px] top-0" data-name="Mask group" data-node-id="1:178">
+				<div className="absolute left-[432px] top-0" data-name="Mask group" data-node-id="1:178">
 					<div
 						className="absolute bg-center bg-cover bg-no-repeat h-[910.843px] left-[-128px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[560px_13px] mask-size-[402px_874px] top-[-13px] w-[1425px]"
 						data-name="ai_tools._Waist-up_portrait_American_shot_of_a_smiling_person_w_47b472c7-205a-4d66-bacf-38f70faa6d81 2"
@@ -63,7 +65,7 @@ function BackMobile({ propriedade1 = "Frame 2" }: BackMobileProps) {
 						style={{ backgroundImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D814}')`, maskImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D813}')` }}
 					/>
 				</div>
-				<div className="absolute contents left-[864px] top-0" data-name="Mask group" data-node-id="1:181">
+				<div className="absolute left-[864px] top-0" data-name="Mask group" data-node-id="1:181">
 					<div
 						className="absolute bg-center bg-cover bg-no-repeat h-[907.008px] left-[283px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[581px_18px] mask-size-[402px_874px] top-[-18px] w-[1419px]"
 						data-name="ai_tools._Waist-up_portrait_American_shot_of_a_smiling_person_w_47b472c7-205a-4d66-bacf-38f70faa6d81 2"
@@ -71,7 +73,7 @@ function BackMobile({ propriedade1 = "Frame 2" }: BackMobileProps) {
 						style={{ backgroundImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D815}')`, maskImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D813}')` }}
 					/>
 				</div>
-				<div className="absolute contents left-[1296px] top-0" data-name="Mask group" data-node-id="1:190">
+				<div className="absolute left-[1296px] top-0" data-name="Mask group" data-node-id="1:190">
 					<div
 						className="absolute bg-center bg-cover bg-no-repeat h-[907.008px] left-[1015px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[281px_18px] mask-size-[402px_874px] top-[-18px] w-[1419px]"
 						data-name="ai_tools._Waist-up_portrait_American_shot_of_a_smiling_person_w_47b472c7-205a-4d66-bacf-38f70faa6d81 2"
@@ -79,7 +81,7 @@ function BackMobile({ propriedade1 = "Frame 2" }: BackMobileProps) {
 						style={{ backgroundImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D816}')`, maskImage: `url('${imgAiToolsWaistUpPortraitAmericanShotOfASmilingPersonW47B472C7205A4D66Bacf38F70Faa6D813}')` }}
 					/>
 				</div>
-				<div className="absolute contents left-[1728px] top-0" data-name="Mask group" data-node-id="1:193">
+				<div className="absolute left-[1728px] top-0" data-name="Mask group" data-node-id="1:193">
 					<div
 						className="absolute bg-center bg-cover bg-no-repeat h-[907.008px] left-[1099px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[629px_18px] mask-size-[402px_874px] top-[-18px] w-[1419px]"
 						data-name="ai_tools._Waist-up_portrait_American_shot_of_a_smiling_person_w_47b472c7-205a-4d66-bacf-38f70faa6d81 2"
@@ -122,8 +124,8 @@ function Acordeon({ propriedade1 = "Retraido" }: AcordeonProps) {
 
 export default function Mobile() {
 	return (
-		<div className="bg-white relative size-full" data-name="Mobile" data-node-id="1:75">
-			<div className="absolute contents left-0 top-0" data-name="cabecalho" data-node-id="1:76">
+		<div className="bg-white relative w-full" style={{ height: 5750 }} data-name="Mobile" data-node-id="1:75">
+			<div className="absolute left-0 top-0" data-name="cabecalho" data-node-id="1:76">
 				<div className="absolute bg-center bg-cover bg-no-repeat h-[87px] left-0 top-0 w-[402px]" data-name="em cima" data-node-id="1:77" style={{ backgroundImage: `url('${imgEmCima}')` }} />
 				<div className="absolute h-[36px] top-[18px] w-[315px]" data-name="Botao call" data-node-id="1:138" style={{ left: "calc(8.333% + 29.5px)" }}>
 					<BotaoCall />
@@ -132,7 +134,7 @@ export default function Mobile() {
 					<img alt="" className="block max-w-none size-full" src={imgLogoSetima} />
 				</div>
 			</div>
-			<div className="absolute contents left-0 top-[5514px]" data-name="Bloco 08" data-node-id="1:575">
+			<div className="absolute left-0 top-[5514px]" data-name="Bloco 08" data-node-id="1:575">
 				<div className="absolute bg-[#13171a] h-[200px] left-0 top-[5514px] w-[402px]" data-node-id="1:467" />
 				<div className="absolute font-['Ubuntu:Regular',_sans-serif] h-[27px] leading-[0] not-italic text-[#f8f8f2] text-[12px] text-center top-[5635.62px] translate-x-[-50%] w-[351px]" data-node-id="17:77" style={{ left: "calc(50% + 0.5px)" }}>
 					<p className="leading-[normal]">@2025 Setima. Todos os direitos reservados. Todo o conteúdo deste site é protegido por leis de propriedade intelectual.</p>
@@ -177,7 +179,7 @@ export default function Mobile() {
 					</button>
 				</div>
 			</div>
-			<div className="absolute contents left-[-248px] top-[3906px]" data-name="Bloco 06" data-node-id="1:331">
+			<div className="absolute left-[-248px] top-[3906px]" data-name="Bloco 06" data-node-id="1:331">
 				<div className="absolute bg-[#13171a] h-[836px] left-0 top-[3906px] w-[402px]" data-node-id="1:332" />
 				<div className="absolute bg-center bg-cover bg-no-repeat h-[506px] left-[-248px] top-[4156px] w-[898px]" data-name="aquamarine (4) 1" data-node-id="1:339" style={{ backgroundImage: `url('${imgAquamarine41}')` }} />
 				<div className="absolute contents font-['Ubuntu:Bold',_sans-serif] leading-[0] not-italic text-[18px] text-center text-white top-[4610px]" data-node-id="1:338" style={{ left: "calc(33.333% + 1px)" }}>
@@ -195,7 +197,7 @@ export default function Mobile() {
 					<p className="leading-[normal]">O FLUXO NGI: DA ESTRATÉGIA À PRODUÇÃO, CRIATIVIDADE E TÉCNICA ACELERADAS PELA IA.</p>
 				</div>
 			</div>
-			<div className="absolute contents left-0 top-[3458px]" data-name="Bloco 05" data-node-id="1:327">
+			<div className="absolute left-0 top-[3458px]" data-name="Bloco 05" data-node-id="1:327">
 				<div className="absolute bg-[#f8f8f2] h-[448px] left-0 top-[3458px] w-[402px]" data-node-id="1:328" />
 				<div className="absolute font-['Ubuntu:Light',_sans-serif] leading-[0] left-[201px] not-italic text-[#13171a] text-[20px] text-center top-[3761px] translate-x-[-50%] w-[352px]" data-node-id="1:329">
 					<p className="leading-[normal]">Integramos criatividade, estratégia e tecnologia em um só fluxo, criando conteúdos relevantes de maneira mais rápida, escalável e impactante.</p>
@@ -206,9 +208,9 @@ export default function Mobile() {
 					<p>EM COMPARAÇÃO A PRODUÇÕES TRADICIONAIS.</p>
 				</div>
 			</div>
-			<div className="absolute contents left-0 top-[2322px]" data-name="Bloco 04" data-node-id="1:311">
+			<div className="absolute left-0 top-[2322px]" data-name="Bloco 04" data-node-id="1:311">
 				<div className="absolute bg-[#13171a] h-[600px] left-0 top-[2858px] w-[402px]" data-name="background" data-node-id="1:312" />
-				<div className="absolute contents left-0 top-[2322px]" data-name="imagem grande" data-node-id="1:313">
+			<div className="absolute left-0 top-[2322px]" data-name="imagem grande" data-node-id="1:313">
 					<div
 						className="absolute bg-center bg-cover bg-no-repeat h-[547px] left-[-234px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[234px_10px] mask-size-[402px_536px] top-[2312px] w-[912px]"
 						data-name="25_0127_Almap_Still_TeraTheTown_Frente_V08 1"
@@ -216,7 +218,7 @@ export default function Mobile() {
 						style={{ backgroundImage: `url('${img250127AlmapStillTeraTheTownFrenteV081}')`, maskImage: `url('${img250127AlmapStillTeraTheTownFrenteV82}')` }}
 					/>
 				</div>
-				<div className="absolute contents left-[24px] top-[2889px]" data-name="imagens carrossel" data-node-id="1:316">
+			<div className="absolute left-[24px] top-[2889px]" data-name="imagens carrossel" data-node-id="1:316">
 					<div className="absolute bg-[#d9d9d9] left-[24px] size-[74px] top-[2889px]" data-node-id="1:317" />
 					<div className="absolute bg-[#d9d9d9] size-[74px] top-[2889px]" data-node-id="1:318" style={{ left: "calc(25% + 16.5px)" }} />
 					<div className="absolute bg-[#d9d9d9] size-[74px] top-[2889px]" data-node-id="1:319" style={{ left: "calc(50% + 9px)" }} />
@@ -234,7 +236,7 @@ export default function Mobile() {
 					<p className="font-['Ubuntu:Bold',_sans-serif]">SEU BUDGET OTIMIZADO AO MÁXIMO.</p>
 				</div>
 			</div>
-			<div className="absolute contents left-0 top-[1648px]" data-name="Bloco 03" data-node-id="1:306">
+			<div className="absolute left-0 top-[1648px]" data-name="Bloco 03" data-node-id="1:306">
 				<div className="absolute bg-[#f8f8f2] h-[674px] left-0 top-[1648px] w-[402px]" data-name="background" data-node-id="1:307" />
 				<div className="absolute font-['Ubuntu:Light',_sans-serif] leading-[0] left-[200px] not-italic text-[#13171a] text-[20px] text-center top-[2171px] translate-x-[-50%] w-[352px]" data-node-id="1:309">
 					<p className="leading-[normal]">O 3D já trascende o universo de stills e CGI: tornou-se linguagem, experiência e presença digital que é capaz de capturar e converter.</p>
@@ -245,7 +247,7 @@ export default function Mobile() {
 					<p className="font-['Ubuntu:Bold',_sans-serif]">+66% DE ENGAJAMENTO, +9% EM VENDAS.</p>
 				</div>
 			</div>
-			<div className="absolute contents left-0 top-[874px]" data-name="Bloco 02" data-node-id="1:297">
+			<div className="absolute left-0 top-[874px]" data-name="Bloco 02" data-node-id="1:297">
 				<div className="absolute bg-[#13171a] h-[774px] left-0 top-[874px] w-[402px]" data-name="background" data-node-id="1:298" />
 				<div className="absolute font-['Ubuntu:Light',_sans-serif] leading-[0] left-[201px] not-italic text-[20px] text-center text-white top-[1399px] translate-x-[-50%] w-[354px]" data-node-id="1:299">
 					<p className="leading-[normal]">Como uma content-tech, podemos atuar desde o conceito criativo até a distribuição multicanal e a análise de desempenho. Um fluxo sustentado por um stack tecnológico integrado que vai de IA à automação, do CMS/DAM ao analytics e além.</p>
@@ -269,11 +271,11 @@ export default function Mobile() {
 					<p className="leading-[normal]">vídeo</p>
 				</div>
 			</div>
-			<div className="absolute contents left-0 top-0" data-name="Bloco 01" data-node-id="1:172">
+			<div className="absolute left-0 top-0" data-name="Bloco 01" data-node-id="1:172">
 				<div className="absolute h-[874px] left-0 overflow-clip top-0 w-[402px]" data-name="back mobile" data-node-id="1:280">
 					<BackMobile />
 				</div>
-				<div className="absolute contents left-0 top-[701px]" data-name="Transparências" data-node-id="1:145">
+			<div className="absolute left-0 top-[701px]" data-name="Transparências" data-node-id="1:145">
 					<div className="absolute flex h-[173px] items-center justify-center left-0 top-[701px] w-[402px]">
 						<div className="flex-none scale-y-[-100%]">
 							<div className="bg-center bg-cover bg-no-repeat h-[173px] w-[402px]" data-name="embaixo" data-node-id="1:146" style={{ backgroundImage: `url('${imgEmbaixo}')` }} />
--- src/content/youtubeIds.ts
+++
@@ -1,5 +0,0 @@
-// Placeholders para os IDs do YouTube. Você enviará os definitivos depois.
-export const YT = {
-  heroSecondary: 'REPLACE_ME_1', // Bloco 02
-  section3: 'REPLACE_ME_2',      // Bloco 03
-};
